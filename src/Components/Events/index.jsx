e3fc51363a669eee556837d3de53f1e4108b6723
hnrqss <hnrq.assuncao@gmail.com>
2023-02-28 15:22:45 -0300

    Corrigindo Key prop

--- src/Components/Events/index.jsx
+++ src/Components/Events/index.jsx
@@ -8,8 +8,7 @@ export function Events({ events = []}) {
     return (
         <ul className='events-list'>
         {events?.map((event, index) => (
-          <li key={index} className='event-item'>
-              {console.log(event)}
+          <li key={event?.match?.id} className='event-item'>
               <Link to={event?.state === 'inProgress' ? `/partida/${event?.match?.id}` : ''}>
                 <div className='event-header'>
                     <span>{event?.league?.name}</span>
@@ -17,13 +16,13 @@ export function Events({ events = []}) {
                 </div>
                 <div className='event-body'>
                   {event?.match?.teams?.map((team, i) => (
-                    <>
+                    <div className='team-container' key={i}>
                       <div className='team-place'>
                         <img src={team?.image?.replace('http://', 'https://')} />
                         <span>{team?.code}</span>
                       </div>                        
                       <span>{i === 0 && 'VS'}</span>
-                    </>
+                    </div>
                   ))}
                 </div>
               </Link>
